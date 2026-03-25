6382e62c15e95171b2ef0e817057d813c76e7662
Junior Ferreira <juniorffer45@gmail.com>
2026-03-25 15:26:28 -0300

    Update BancoDeDados.sql

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -7,8 +7,10 @@ CREATE TABLE eventos (
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Índice para busca rápida por nome
+-- Índice para busca rápida por Nome
 CREATE INDEX idx_nome_evento ON eventos(nome);
+-- Índice para busca rápida por Data
+CREATE INDEX idx_eventos_data ON eventos(data_evento);
 
 -- Restrição: Não permitir eventos com nomes vazios
 ALTER TABLE eventos ADD CONSTRAINT check_nome_valido CHECK (LENGTH(nome) > 0);
