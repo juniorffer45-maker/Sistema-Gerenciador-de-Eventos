2b441bbe7fcc574fa4ae526d5ba7a721f5e82261
Junior Ferreira <juniorffer45@gmail.com>
2026-03-26 20:55:47 -0300

    Update BancoDeDados.sql

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -4,7 +4,7 @@ CREATE TABLE eventos (
     data TIMESTAMP NOT NULL,
     local VARCHAR(255) NOT NULL,
     descricao TEXT,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Índice para busca rápida por Nome
